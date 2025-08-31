93e4db192794b7847eda80c77d5a24bb09201b61
Thomas Luo <25195389+tluocs@users.noreply.github.com>
2025-08-31 18:54:48 -0400

    Add files via upload

---
+++ NetworkMetrics/one-hop-nano-pi/metricsTest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Query orginators"
+sudo batctl o | tee $1
+echo "Query ping"
+sudo batctl ping -R d8:3a:dd:21:c1:92 -c 20 | tee -a $1
+echo "Query throughput"
+sudo batctl tp d8:3a:dd:21:c1:92 -t 20000 | tee -a $1 
+echo "Finish"
